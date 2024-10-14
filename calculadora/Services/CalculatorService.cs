639637ef5cefa58e7cf8ecab0dad4434e8d2a1ca
DinisSimoes <di.f.simoes@gmail.com>
2024-10-14 13:06:15 -0300

    first commit
    first commit

---
+++ calculadora/Services/CalculatorService.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Grpc.Core;
+
+namespace calculadora.Services
+{
+    public class CalculatorService : Calculator.CalculatorBase
+    {
+        public override Task<CalculatorResponse> Add(CalculatorRequest request, ServerCallContext context)
+        {
+            var result = request.Number1 + request.Number2;
+            return Task.FromResult(new CalculatorResponse { Result = result });
+        }
+
+        public override Task<CalculatorResponse> Subtract(CalculatorRequest request, ServerCallContext context)
+        {
+            var result = request.Number1 - request.Number2;
+            return Task.FromResult(new CalculatorResponse { Result = result });
+        }
+
+        public override Task<CalculatorResponse> Multiply(CalculatorRequest request, ServerCallContext context)
+        {
+            var result = request.Number1 * request.Number2;
+            return Task.FromResult(new CalculatorResponse { Result = result });
+        }
+
+        public override Task<CalculatorResponse> Divide(CalculatorRequest request, ServerCallContext context)
+        {
+            var result = request.Number1 / request.Number2;
+            return Task.FromResult(new CalculatorResponse { Result = result });
+        }
+    }
+}
